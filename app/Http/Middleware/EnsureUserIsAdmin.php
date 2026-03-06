2df22d6de62bb73cab4340d5b732b00a0ee3ab67
EdineiLuizPereiraJr <edineijr2005@gmail.com>
2026-03-05 22:26:41 -0300

    verification user is admin or not

---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+    // Verifique a regra de admin de acordo com a sua implementação (ex: role, is_admin, etc)
+    if (! $request->user() || ! $request->user()->hasRole('admin')) { // Exemplo com Spatie Permission
+        return response()->json(['message' => 'Unauthorized. Admin access required.'], 401); // Retornando 401 como pedido na issue
+    }
+
+        return $next($request);
+    }
+}
