e52b1ca7811a566311cda48f77d8857b9d2f28ec
EdineiLuizPereiraJr <edineijr2005@gmail.com>
2026-03-11 17:27:18 -0300

    route_file_format

--- routes/api.php
+++ routes/api.php
@@ -18,22 +18,25 @@ Route::prefix('/v1')->group(function () {
 
     //Rotas Autenticadas
     Route::middleware('auth:sanctum')->group(function () {
-    //listagem Publica
-    Route::get('/authors', [AuthorController::class, 'index']);
-    //Rotas Autenticadas
-    Route::middleware('auth:sanctum')->group(function () {
 
-        Route::post('/auth/logout', [AuthController::class, 'logout']);
-        Route::put('/users/{user}', [UserController::class, 'update']);
-        Route::delete('/users/{user}', [UserController::class, 'destroy']);
+        //listagem Publica
+        Route::get('/authors', [AuthorController::class, 'index']);
+        //Rotas Autenticadas
+        Route::middleware('auth:sanctum')->group(function () {
 
-        //Rotas administrativas
-        Route::middleware(['admin'])->prefix('admin')->group(function () {
-            Route::post('/studios', [StudioController::class, 'store']);
-            Route::put('/studios/{studio}', [StudioController::class, 'update']);
-            Route::delete('/studios/{studio}', [StudioController::class, 'destroy']);
-            Route::post('/authors', [AuthorController::class, 'store']);
-            Route::put('/authors/{author}', [AuthorController::class, 'update']);
-            Route::delete('/authors/{author}', [AuthorController::class, 'destroy']);
+            Route::post('/auth/logout', [AuthController::class, 'logout']);
+            Route::put('/users/{user}', [UserController::class, 'update']);
+            Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+            //Rotas administrativas
+            Route::middleware(['admin'])->prefix('admin')->group(function () {
+                Route::post('/studios', [StudioController::class, 'store']);
+                Route::put('/studios/{studio}', [StudioController::class, 'update']);
+                Route::delete('/studios/{studio}', [StudioController::class, 'destroy']);
+                Route::post('/authors', [AuthorController::class, 'store']);
+                Route::put('/authors/{author}', [AuthorController::class, 'update']);
+                Route::delete('/authors/{author}', [AuthorController::class, 'destroy']);
+            });
+        });
     });
 });
